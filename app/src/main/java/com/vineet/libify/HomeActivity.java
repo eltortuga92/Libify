a3e8b908dd11510a4dd87b3d7c9a44389945e599
Vineet Kaushik <vineetkaushik053@gmail.com>
2020-02-22 16:02:35 -0500

    Added RecyclerView to HomeActivity

--- app/src/main/java/com/vineet/libify/HomeActivity.java
+++ app/src/main/java/com/vineet/libify/HomeActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.View;
@@ -17,6 +19,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = HomeActivity.class.getSimpleName();
     private LinkedList<String> mCategoryList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private CategoryListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,14 @@ public class HomeActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             mCategoryList.addLast("Category" + i);
         }
+        // Get a handle to RecyclerView
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed
+        mAdapter = new CategoryListAdapter(this, mCategoryList);
+        // Connect the adapter with the RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
